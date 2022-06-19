4f0d4e570fb77c4e42f8f3bcaeaa34fe4c54ebba
Ana Gabriela <anagabrieladev@gmail.com>
2022-06-19 01:56:59 -0300

    test: return 400 if no valid email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,13 +1,26 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 import { SignUpController } from './signup'
 
+const makeEmailValidatorStub = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
+
 interface SutTypes {
+  emailValidator: EmailValidator
   sut: SignUpController
 }
 const makeSut = (): SutTypes => {
-  const sut = new SignUpController()
+  const emailValidator = makeEmailValidatorStub()
+  const sut = new SignUpController(emailValidator)
 
-  return { sut }
+  return { sut, emailValidator }
 }
 
 describe('SignUpController', () => {
@@ -54,5 +67,38 @@ describe('SignUpController', () => {
 
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new MissingParamError('password'))
+  })
+
+  it('should call EmailValidator with correct params', async () => {
+    const { sut, emailValidator } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password'
+      }
+    }
+    const emailValidatorSpy = jest.spyOn(emailValidator, 'isValid')
+
+    await sut.handle(httpRequest)
+
+    expect(emailValidatorSpy).toHaveBeenCalledWith('any_mail@mail.com')
+  })
+
+  it('should return 400 if invalid email is provided', async () => {
+    const { sut, emailValidator } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalidMail@mail.com',
+        password: 'any_password'
+      }
+    }
+    jest.spyOn(emailValidator, 'isValid').mockReturnValue(false)
+
+    const response = await sut.handle(httpRequest)
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new Error('Email is not valid'))
   })
 })
