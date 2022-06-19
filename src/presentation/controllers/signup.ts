e750fecf74a8d45ff781b1f61b592e528a14f4b9
Ana Gabriela <anagabrieladev@gmail.com>
2022-06-19 01:56:46 -0300

    feat: return 400 if no valid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,13 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helpers'
+import { EmailValidator } from '../protocols/email-validator'
 import { Controller, HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'password']
     const { body } = httpRequest
@@ -12,6 +17,9 @@ export class SignUpController implements Controller {
 
       return badRequest(new MissingParamError(field))
     }
+
+    const isValidMail = this.emailValidator.isValid(body.email)
+    if (!isValidMail) return badRequest(new Error('Email is not valid'))
 
     return {
       statusCode: 200,
