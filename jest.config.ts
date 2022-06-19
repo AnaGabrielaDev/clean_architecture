d064c60b787bca522c36a723e36a42390d74d250
Ana Gabriela <anagabrieladev@gmail.com>
2022-06-19 00:31:11 -0300

    fix: wrong jest config

--- jest.config.ts
+++ jest.config.ts
@@ -124,12 +124,12 @@ export default {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: ['<rootDir>/src'],
+  // rootDir: '<rootDir>/src',
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    '<rootDir>/src'
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
